2ba2aa49df740683d364b72792a6140d3daeaac3
Monteiro Steed <steed.monteiro@gmail.com>
2023-10-31 12:23:52 +0100

    fix: colors

--- src/providers/Theme.tsx
+++ src/providers/Theme.tsx
@@ -10,6 +10,19 @@ type VariantColorConfig = Record<string, Record<string, string>>;
 const defaultColors: ColorConfig = {
   white: '#FFFFFF',
   black: '#000000',
+  lightGrey: 'rgba(224, 224, 224, 1)',
+  midGrey: 'rgba(158, 158, 158, 1)',
+  darkBlueGrey: 'rgba(47, 72, 88, 1)',
+  subGrey: 'rgba(66, 66, 66, 1)',
+  fieldColor: 'rgba(250, 250, 250, 1)',
+  textDisabled: 'rgba(115, 115, 115, 1)',
+  primary: 'rgba(249, 115, 22, 1)',
+  secondary: 'rgba(8, 145, 178, 1)',
+  success: 'rgba(34, 197, 94, 1)',
+  error: 'rgba(239, 68, 68, 1)',
+  warning: ' rgba(233, 176, 19, 1)',
+  disabled: 'rgba(158, 158, 158, 0.38)',
+  loading: 'rgba(158, 158, 158, 1)',
 };
 
 export const ThemeContext = createContext<{
