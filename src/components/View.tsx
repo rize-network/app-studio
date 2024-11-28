ec5f77f431c4dfeb4985fb435b77b109aabcf210
Monteiro Steed <steed.monteiro@gmail.com>
2024-11-28 22:43:14 +0100

    chore: release v0.5.16

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { forwardRef, memo, ComponentPropsWithRef } from 'react';
 import { Element, ElementProps } from './Element';
 import { ViewStyleProps } from '../types/style';
 import { CSSProperties } from 'react';
 
+type ForwardRefComponent<
+  Props,
+  Ref extends HTMLElement = HTMLElement,
+> = React.ForwardRefExoticComponent<
+  Props & ViewProps & React.RefAttributes<Ref>
+>;
 // interface ScrollProps extends ViewProps {
 //   // Définir des props spécifiques pour Scroll si nécessaire
 // }
@@ -10,6 +16,17 @@ import { CSSProperties } from 'react';
 // interface SpanProps extends Omit<ViewProps, 'as'> {
 //   // Définir des props spécifiques pour Span si nécessaire
 // }
+
+// Fonction utilitaire pour combiner memo et forwardRef
+export function memoForwardRef<Props, Ref extends HTMLElement = HTMLElement>(
+  component: React.ForwardRefRenderFunction<Ref, Props>
+): ForwardRefComponent<Props, Ref> {
+  const ForwardedComponent = forwardRef(component);
+  const MemoizedComponent = memo(
+    ForwardedComponent
+  ) as unknown as ForwardRefComponent<Props, Ref>;
+  return MemoizedComponent;
+}
 
 export interface ViewProps
   extends Omit<
@@ -21,7 +38,9 @@ export interface ViewProps
   onPress?: (..._args: any) => void;
 }
 
-export const View = React.memo((props: ViewProps) => <Element {...props} />);
+export const View = memoForwardRef<ComponentPropsWithRef<typeof Element>>(
+  (props, ref) => <Element {...props} ref={ref} />
+);
 
 export const Div = React.memo((props: ViewProps) => <View {...props} />);
 
